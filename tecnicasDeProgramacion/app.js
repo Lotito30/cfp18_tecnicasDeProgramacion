af205660f4fefa034dc9bca50e2d2c7caf8c21e5
Lotito30 <dominguezje21@gmail.com>
2023-03-29 11:02:59 -0300

    actualice variables de require

--- tecnicasDeProgramacion/app.js
+++ tecnicasDeProgramacion/app.js
@@ -6,12 +6,12 @@ const jesusD = require("./clase_virtual_15_03/jesus/jesus")
 console.log("Mi nombre es: "+jesusD[0] + " "+jesusD[1]+ " y "+ jesusD[2]);
 
 // clase 22/03 calculadora
-const suma = require("./clase_virtual_22_03/sumar")
-const resta = require("./clase_virtual_22_03/restar")
-const multiplicacion = require("./clase_virtual_22_03/multiplicar")
-const division = require("./clase_virtual_22_03/division")
+const variableSuma = require("./clase_virtual_22_03/sumar")
+const variableResta = require("./clase_virtual_22_03/restar")
+const variableMultiplicacion = require("./clase_virtual_22_03/multiplicar")
+const variableDivision = require("./clase_virtual_22_03/division")
 
-console.log("La suma de los valores es: " + suma);
-console.log("La resta de los valores es: " + resta);
-console.log("La multiplicacion de los valores es: " + multiplicacion);
-console.log("La division de los valores es: " + division);
+console.log("La suma de los valores es: " + variableSuma);
+console.log("La resta de los valores es: " + variableResta);
+console.log("La multiplicacion de los valores es: " + variableMultiplicacion);
+console.log("La division de los valores es: " + variableDivision);
